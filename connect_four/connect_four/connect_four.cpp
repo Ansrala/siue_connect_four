eb5df4462b0e0f20f7c36e66b7281b374c6042b1
Ansrala <jmooreftw@gmail.com>
2013-04-17 16:25:45 -0500

    First commit

---
+++ connect_four/connect_four/connect_four.cpp
@@ -0,0 +1,18 @@
+// connect_four.cpp : main project file.
+
+#include "stdafx.h"
+#include "Form1.h"
+
+using namespace connect_four;
+
+[STAThreadAttribute]
+int main(array<System::String ^> ^args)
+{
+	// Enabling Windows XP visual effects before any controls are created
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false); 
+
+	// Create the main window and run it
+	Application::Run(gcnew Form1());
+	return 0;
+}
