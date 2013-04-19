fd8bc103943138e45fc8e2dd98cac59d4f21b115
Ansrala <jmooreftw@gmail.com>
2013-04-19 03:10:17 -0500

    deleted c++ adding c#

--- connect_four/connect_four/stdafx.cpp
+++
@@ -1,7 +0,0 @@
-// stdafx.cpp : source file that includes just the standard includes
-// connect_four.pch will be the pre-compiled header
-// stdafx.obj will contain the pre-compiled type information
-
-#include "stdafx.h"
-
-
